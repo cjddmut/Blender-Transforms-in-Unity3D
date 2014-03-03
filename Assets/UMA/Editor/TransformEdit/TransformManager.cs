1c8d79784fb5955d76e387fa6631d078a7aa12a9
C.J. Kimberlin <cjkimberlin0@gmail.com>
2014-03-03 04:23:10 -0800

    SceneView now gains focus when hotkeys are pressed in the hierarchy and if in a mode, focus is given by to sceneview if lost

--- Assets/UMA/Editor/TransformEdit/TransformManager.cs
+++ Assets/UMA/Editor/TransformEdit/TransformManager.cs
@@ -11,19 +11,58 @@ namespace UMA
         private static Rotate _RotateEdit;
         private static Scale _ScaleEdit;
         private static ModalEdit _ActiveModal;
+        private static ModalEdit _DelayStart;
 
         private static bool _SwallowMouse = false;
         private static int _MouseButton;
+
+        private static int _SceneViewControlID;
 
         // Use this for initialization
         static TransformManager()
         {
             SceneView.onSceneGUIDelegate += SceneGUI;
+            EditorApplication.hierarchyWindowItemOnGUI += HierarchyOnGUI;
 
             // Create our model edit singletons.
             _TranslateEdit = new Translate();
             _RotateEdit = new Rotate();
             _ScaleEdit = new Scale();
+        }
+
+        static void HierarchyOnGUI(int i, Rect r)
+        {
+            if (!Data.TransformEditingEnabled)
+            {
+                return;
+            }
+
+            if (Event.current.type == EventType.KeyDown)
+            {
+                if (Event.current.keyCode == Data.TranslateKey)
+                {
+                    SceneView.lastActiveSceneView.Focus();
+                    Event.current.Use();
+
+                    // Hey translate! We'll start it on next SceneGUI!
+                    _DelayStart = _TranslateEdit;
+                }
+                
+                if (Event.current.keyCode == Data.RotateKey)
+                {
+                    SceneView.lastActiveSceneView.Focus();
+                    Event.current.Use();
+                    _DelayStart = _RotateEdit;
+                }
+
+                if (Event.current.keyCode == Data.ScaleKey)
+                {
+                    SceneView.lastActiveSceneView.Focus();
+                    Event.current.Use();
+                    _DelayStart = _ScaleEdit;
+                }
+
+            }
         }
 
         static void SceneGUI(SceneView sceneView)
@@ -37,9 +76,29 @@ namespace UMA
             {
                 _ActiveModal.Update();
 
+                if (EditorWindow.focusedWindow != sceneView)
+                {
+                    // SceneView lost focus but we're in a mode so we force it back.
+                    sceneView.Focus();
+                }
+
                 // We force the scene to continue to update if we are in a mode.
                 HandleUtility.Repaint();
             }
+
+            if (_DelayStart != null)
+            {
+                // We got a message to start!
+                if (_ActiveModal != null)
+                {
+                    _ActiveModal.Cancel();
+                }
+
+                _ActiveModal = _DelayStart;
+                _DelayStart = null;
+                _ActiveModal.Start();
+            }
+
 
             if (Event.current.isKey && Event.current.type == EventType.KeyDown)
             {
