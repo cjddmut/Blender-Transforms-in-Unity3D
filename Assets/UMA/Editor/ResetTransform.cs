bd9632251e1b9e136494a3892111a85b10fb7bac
C.J. Kimberlin <cjkimberlin0@gmail.com>
2014-03-03 00:52:24 -0800

    Removed unnecessary characters in the MenuItem string of ResetTransform

--- Assets/UMA/Editor/ResetTransform.cs
+++ Assets/UMA/Editor/ResetTransform.cs
@@ -6,7 +6,7 @@ namespace UMA
 {
     public class ResetTransform
     {
-        [MenuItem(Data.PACKAGE_NAME + "/Reset/Position _&g", priority = Data.RESET_TRANSFORM_PRIORITY)]
+        [MenuItem(Data.PACKAGE_NAME + "/Reset/Position &g", priority = Data.RESET_TRANSFORM_PRIORITY)]
         static void ResetPosition()
         {
             Undo.RecordObjects(Selection.transforms, "Reset Position");
@@ -17,13 +17,13 @@ namespace UMA
             }
         }
 
-        [MenuItem(Data.PACKAGE_NAME + "/Reset/Position _&g", validate = true)]
+        [MenuItem(Data.PACKAGE_NAME + "/Reset/Position &g", validate = true)]
         static bool ResetPositionCheck()
         {
             return Data.ResetTransformsEnabled;
         }
 
-        [MenuItem(Data.PACKAGE_NAME + "/Reset/Rotation _&r", priority = Data.RESET_TRANSFORM_PRIORITY)]
+        [MenuItem(Data.PACKAGE_NAME + "/Reset/Rotation &r", priority = Data.RESET_TRANSFORM_PRIORITY)]
         static void ResetRotation()
         {
             Undo.RecordObjects(Selection.transforms, "Reset Rotation");
@@ -35,13 +35,13 @@ namespace UMA
 
         }
 
-        [MenuItem(Data.PACKAGE_NAME + "/Reset/Rotation _&r", validate = true)]
+        [MenuItem(Data.PACKAGE_NAME + "/Reset/Rotation &r", validate = true)]
         static bool ResetRotationCheck()
         {
             return Data.ResetTransformsEnabled;
         }
 
-        [MenuItem(Data.PACKAGE_NAME + "/Reset/Scale _&s", priority = Data.RESET_TRANSFORM_PRIORITY)]
+        [MenuItem(Data.PACKAGE_NAME + "/Reset/Scale &s", priority = Data.RESET_TRANSFORM_PRIORITY)]
         static void ResetScale()
         {
             Undo.RecordObjects(Selection.transforms, "Reset Scale");
@@ -52,14 +52,14 @@ namespace UMA
             }
         }
 
-        [MenuItem(Data.PACKAGE_NAME + "/Reset/Scale _&s", validate = true)]
+        [MenuItem(Data.PACKAGE_NAME + "/Reset/Scale &s", validate = true)]
         static bool ResetScaleCheck()
         {
             return Data.ResetTransformsEnabled;
         }
 
         // TODO: Right now this is just sort of lumped in with the Reset Transform functions.
-        [MenuItem(Data.PACKAGE_NAME + "/Create Empty Child _%&n", priority = Data.OTHERS_PRIORITY)]
+        [MenuItem(Data.PACKAGE_NAME + "/Create Empty Child %&n", priority = Data.OTHERS_PRIORITY)]
         static void CreateEmptyChild()
         {
             // TODO: Where does it make sense to place the child? Default is 0, 0, 0 in world space.
@@ -74,7 +74,7 @@ namespace UMA
             Undo.RegisterCreatedObjectUndo(empty, "Create Empty Child");
         }
 
-        [MenuItem(Data.PACKAGE_NAME + "/Create Empty Child _%&n", validate = true)]
+        [MenuItem(Data.PACKAGE_NAME + "/Create Empty Child %&n", validate = true)]
         static bool CreateEmptyChildCheck()  
         {
             return Data.ResetTransformsEnabled;
